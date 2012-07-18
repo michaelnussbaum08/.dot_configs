d7ad734883c3070fc651b36dd57db8799cde86a6
Michael Nussbaum <michaelnussbaum08@gmail.com>
2012-07-18 09:21:16 -0500

    Updated makes_aliases and added tmux motion shortcuts

--- make_aliases.sh
+++ make_aliases.sh
@@ -3,7 +3,7 @@ ln -s ~/.dot_configs/.gitconfig ~/.gitconfig
 ln -s ~/.dot_configs/.gitignore_global ~/.gitignore_global
 ln -s ~/.dot_configs/.offlineimaprc ~/.offlineimaprc
 ln -s ~/.dot_configs/.pryrc ~/.pryrc
-ln -s ~/.dot_configs/.pythonrc .pythonrc
+ln -s ~/.dot_configs/.pythonrc ~/.pythonrc
 ln -s ~/.dot_configs/.tmux.conf ~/.tmux.conf
 ln -s ~/.dot_configs/.vim ~/.vim
 ln -s ~/.dot_configs/.vim/.vimrc ~/.vimrc
